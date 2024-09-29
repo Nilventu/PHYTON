f3ad030bc34aaea0464b8cdf8413071f6fb0cbe2
Nilventu <nil.venturas@campus.xaloc.org>
2024-09-29 14:42:49 +0200

    Add files via upload

---
+++ PHYTON/15 formativa phyton.py
@@ -0,0 +1,9 @@
+#15. Utiliza el valor Pi de la librería math para calcular el área y volumen de un cilindro,introduciendo por teclado el valor de radio y altura. Resultado con 2 decimales.
+import math
+radio=float(input("¿cuanto mide el radio?: "))
+altura=float(input("¿cuanto mide la altura?: "))
+volumen=math.pi*radio**2*altura
+area=2*math.pi*radio*altura+2*math.pi*radio**2
+print("El área de un cilindro es: ",round(area,2))
+print("El volumen de un cilindro es: ",round(volumen,2))
+
