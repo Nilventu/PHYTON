ca286c2e6ca5528aafc54c40be644aeca0fe9f5d
Nilventu <nil.venturas@campus.xaloc.org>
2024-10-18 12:12:27 +0200

    Delete PHYTON/11 formativa phyton.py

--- PHYTON/11 formativa phyton.py
+++
@@ -1,6 +0,0 @@
-#11. Realiza un programa que introduciendo el valor del lado de un cuadrado nos devuelva por pantalla en el área y el perímetro
-lado=float(input("Introduce el largo de un lado del cuadrado: "))
-area=lado*lado
-perimetro=lado*4
-print("El area és: ", area)
-print("El perimetro és: ", perimetro)
