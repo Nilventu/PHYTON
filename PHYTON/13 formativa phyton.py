244562cecd7babf14178e3d856bbd9f8eef5ba49
Nilventu <nil.venturas@campus.xaloc.org>
2024-10-18 12:12:44 +0200

    Delete PHYTON/13 formativa phyton.py

--- PHYTON/13 formativa phyton.py
+++
@@ -1,6 +0,0 @@
-#13. Realiza un programa que, a partir introducir el lado de un cubo, presente por pantalla el área y para calcular el volumen utiliza el operador de exponente.
-lado=int(input("introduce un lado del cubo: "))
-area_cubo=lado**2
-volumen_cubo=lado**3
-print("El área del cubo es:", area_cubo)
-print("El volumen del cubo es:", volumen_cubo)
