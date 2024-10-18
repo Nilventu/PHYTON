c1e7bce7fe11e50ea4bb70a21f4c40eb1f25def1
Nilventu <nil.venturas@campus.xaloc.org>
2024-10-18 12:13:56 +0200

    Delete PHYTON/6 formativa phyton.py

--- PHYTON/6 formativa phyton.py
+++
@@ -1,8 +0,0 @@
-#6. A partir del programa 5. Haz que se muestre por pantalla también la frase en el orden inverso en que se han introducido las palabras.
-var1=input ( "Introduzca la primera palabra: ")
-var2=input ( "Introduzca la segunda palabra: ")
-var3=input ( "Introduzca la tercera palabra: ")
-var4=input ( "Introduzca la cuarta palabra: ")
-var5=input ( "Introduzca la quinta palabra: ")
-print(var5+var4+var3+var2+var1)
-
