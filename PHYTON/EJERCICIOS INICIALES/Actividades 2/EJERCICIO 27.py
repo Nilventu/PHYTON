12315ca6728c325958015299a724458c62ae2924
Nilventu <nil.venturas@campus.xaloc.org>
2024-10-25 12:12:11 +0200

    Add files via upload

---
+++ PHYTON/EJERCICIOS INICIALES/Actividades 2/EJERCICIO 27.py
@@ -0,0 +1,13 @@
+#27. Mejora el programa anterior para controlar que el valor introducido es una letra y en caso de introducir un número, aparezca un aviso por pantalla
+letra= input("Inserta letra o digito: ")
+
+if letra.isupper():
+    print("La letra es mayúscula")
+elif letra.islower():
+    print("La letra es mayúscula")
+elif letra.isdigit():
+    print("El digito es un número")
+
+else:
+    print("¿La letra es mayúscula?")
+    
