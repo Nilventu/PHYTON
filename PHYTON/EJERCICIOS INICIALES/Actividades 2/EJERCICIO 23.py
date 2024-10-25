71f655667a264d39820f22dcf1965185648611d2
Nilventu <nil.venturas@campus.xaloc.org>
2024-10-25 11:54:48 +0200

    EJERCICIO 23.py

--- PHYTON/EJERCICIOS INICIALES/Actividades 2/EJERCICIO 23.py
+++ PHYTON/EJERCICIOS INICIALES/Actividades 2/EJERCICIO 23.py
@@ -1,4 +1,4 @@
-#25. Repite el programa número 23 pero en esta ocasión utilizando operadores lógicos.
+#23. Modifica el programa anterior para establecer si la nota es un excelente (8.5 a 10), un notable (>=6.5 -<8.5), satisfactorio (<6.5 -5) o insuficiente (<5). Controla que la nota introducida esté entre 0 y 10. Utilizar elif sin operadores lógicos.
 nota=float(input("Inserta tu nota: "))
 round(nota, 1)
 if nota>=0 and nota <5:
