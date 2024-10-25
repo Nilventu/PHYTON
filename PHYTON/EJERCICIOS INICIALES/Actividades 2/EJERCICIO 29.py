b4564aee3b0868df1c2aadf69f72761a2802b26c
Nilventu <nil.venturas@campus.xaloc.org>
2024-10-25 12:17:56 +0200

    Add files via upload

---
+++ PHYTON/EJERCICIOS INICIALES/Actividades 2/EJERCICIO 29.py
@@ -0,0 +1,3 @@
+#29. Busca Internet qué función permite obtener la longitud de un String, realiza un programa que al introducir una frase devuelva la longitud
+frase= input("Introduce una frase: ")
+print(len(frase))
