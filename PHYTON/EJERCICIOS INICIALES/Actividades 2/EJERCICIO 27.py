ead1daf82e5641092df307e310d31c3c60a26558
Nilventu <nil.venturas@campus.xaloc.org>
2024-11-03 13:32:41 +0100

    Update EJERCICIO 27.py

--- PHYTON/EJERCICIOS INICIALES/Actividades 2/EJERCICIO 27.py
+++ PHYTON/EJERCICIOS INICIALES/Actividades 2/EJERCICIO 27.py
@@ -4,10 +4,9 @@ letra= input("Inserta letra o digito: ")
 if letra.isupper():
     print("La letra es mayúscula")
 elif letra.islower():
-    print("La letra es mayúscula")
+    print("La letra es minúcula")
 elif letra.isdigit():
     print("El digito es un número")
 
 else:
-    print("¿La letra es mayúscula?")
-    
+    print("¿La letra es mayúscula?") 
