21246739c52e4ddd4771a33e04033c205b8306e8
Nilventu <nil.venturas@campus.xaloc.org>
2025-03-14 11:47:48 +0100

    Delete PHYTON/Ejercicio 51.py

--- PHYTON/Ejercicio 51.py
+++
@@ -1,5 +0,0 @@
-#51. A partir del programa anterior, modifica el código para que sea el usuario quién introduzca el número de veces que desea que repita la frase Buenos días. Con While
-int(input()
-while var<2:
-  print("Buenos Dias")
-  var+=1
