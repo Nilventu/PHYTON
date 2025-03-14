c44d681cbc2464e0d5b45939dd403cb2b13c04c3
Nilventu <nil.venturas@campus.xaloc.org>
2025-03-14 11:49:39 +0100

    Delete PHYTON/Ejercicio 60.py

--- PHYTON/Ejercicio 60.py
+++
@@ -1,8 +0,0 @@
-#60. Diseña un programa que al introducir un número, realice su tabla de multiplicar del 1 al 10. Utiliza únicamente el while
-repes=1
-var1=2
-numero= int(input("introduce un numero: "))
-while repes<11:
-    resultado=var1*repes
-    repes+=1
-    print(f"{resultado}")
