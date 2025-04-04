3a3f1b7194a8e3d9bf4075943f6ab5274c392813
Nilventu <nil.venturas@campus.xaloc.org>
2025-04-04 12:14:44 +0200

    Update ejercicio_38.py

--- PHYTON/ejercicios_for_python/ejercicio_38.py
+++ PHYTON/ejercicios_for_python/ejercicio_38.py
@@ -1,13 +1,13 @@
-# Programa que verifica si las notas están en el rango de 0 a 10
-num_notas = int(input("Introduce el número de notas que deseas introducir: "))
+# 38. A partir del programa anterior, establece los rangos para que el usuario no pueda introducir notas inferiores a 0 y superiores a 10
+nota=0
+num_notas = int(input("Introduce el número de notas: "))
 for i in range(num_notas):
-    while True:
-        nota = float(input(f"Introduce la nota {i+1}: "))
-        if 0 <= nota <= 10:
-            if nota >= 5:
-                print("Asignatura aprobada")
-            else:
-                print("Asignatura suspendida")
-            break
+    nota = float(input(f"Introduce la nota {i+1}: "))
+    if nota>=0 and nota<=10:
+        if nota >= 5:
+            print("Asignatura aprobada")
         else:
-            print("Has introducido una nota equivocada.")
+            print("Asignatura suspendida")
+        
+    else:
+        print("Has introducido una nota equivocada.")
