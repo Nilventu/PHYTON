cf6ca25941b16609bba824382fb666a8000dc68a
Nilventu <nil.venturas@campus.xaloc.org>
2025-04-04 20:46:43 +0200

    Update ejercicio_44.py

--- PHYTON/ejercicios_for_python/ejercicio_44.py
+++ PHYTON/ejercicios_for_python/ejercicio_44.py
@@ -1,3 +1,3 @@
-# Recorrer números de 0 a 100 con saltos de 3
+# 44. Realiza un programa que recorra todos los números comprendidos de 0 a 100 realizando saltosde 3 en 3. El resultado debe aparecer por pantalla en una línea con los números separados por ‘,’
 for i in range(0, 101, 3):
     print(i)
