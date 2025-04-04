318c38136a595b021800b1987ba96d020981e963
Nilventu <nil.venturas@campus.xaloc.org>
2025-04-04 20:53:26 +0200

    Update ejercicio_49.py

--- PHYTON/ejercicios_for_python/ejercicio_49.py
+++ PHYTON/ejercicios_for_python/ejercicio_49.py
@@ -1,6 +1,8 @@
-# Indicar en qué posición de la palabra está la letra introducida
-palabra_secreta = input("Introduce la palabra secreta: ")
+#49. A partir del programa anterior, modifica el código para que al introducir la letra por teclado teindique en qué posición de la palabra se encuentra la letra.
+
+palabra = input("Introduce la palabra secreta: ")
 for i in range(len(palabra_secreta)):
+
     letra = input("Introduce una letra: ")
-    if letra == palabra_secreta[i]:
+    if letra == palabra[i]:
         print(f"La letra se encuentra en la posición {i + 1}")
