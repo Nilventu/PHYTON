ac90786ae47bb5cb3fc5efdc4e0cedf848e3f475
Nilventu <nil.venturas@campus.xaloc.org>
2025-04-04 12:39:35 +0200

    Update ejercicio_43.py

--- PHYTON/ejercicios_for_python/ejercicio_43.py
+++ PHYTON/ejercicios_for_python/ejercicio_43.py
@@ -1,4 +1,4 @@
-# Recorrer una palabra y mostrar cada letra con su posición
+# 43. Realiza un programa que recorra con un for una palabra introducida por teclado y se imprima por pantalla cada letra
 palabra = input("Introduce una palabra: ")
 for i in range(len(palabra)):
     print(f"En la posición {i} está la {palabra[i]}")
