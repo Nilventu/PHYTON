595da6349a1141cebf4a244256cd207a618626f8
Nilventu <nil.venturas@campus.xaloc.org>
2025-04-04 20:48:35 +0200

    Update ejercicio_45.py

--- PHYTON/ejercicios_for_python/ejercicio_45.py
+++ PHYTON/ejercicios_for_python/ejercicio_45.py
@@ -1,12 +1,14 @@
-# Distinguir entre vocales y consonantes
+# 45. Realiza un programa que permita introducir una palabra por teclado y puedas recorrer el stringdistinguiendo vocales y las consonantes:
 palabra = input("Introduce una palabra: ")
 vocales = "aeiouAEIOU"
 vocales_lista = ""
 consonantes_lista = ""
+
 for letra in palabra:
     if letra in vocales:
         vocales_lista += letra
     else:
         consonantes_lista += letra
+        
 print(f"Las vocales de la palabra {palabra} son: {vocales_lista}")
 print(f"Las consonantes de la palabra {palabra} son: {consonantes_lista}")
