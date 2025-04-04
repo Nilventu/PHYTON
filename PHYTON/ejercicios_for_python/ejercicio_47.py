15735c59780185b5c38e941f34bf99e713017016
Nilventu <nil.venturas@campus.xaloc.org>
2025-04-04 20:51:06 +0200

    Update ejercicio_47.py

--- PHYTON/ejercicios_for_python/ejercicio_47.py
+++ PHYTON/ejercicios_for_python/ejercicio_47.py
@@ -1,9 +1,11 @@
-# Mostrar rango de números según los intervalos introducidos
+#47. Realiza un programa donde el usuario introduzca por teclado 2 intervalos, por pantalla se debemostrar el rango de números teniendo en cuenta que se a<b la secuencia será incremental y si a>bla secuencia en descenso. Respeta el formato de salida
 a = int(input("Introduce el primer intervalo: "))
 b = int(input("Introduce el segundo intervalo: "))
+
 if a < b:
     for i in range(a, b + 1):
-        print(i, end="-")
+        print(i)
+        
 else:
     for i in range(a, b - 1, -1):
-        print(i, end="-")
+        print(i)
