a5c10cb586ccb07981e23f48a9f1172fd23eaa1f
Nilventu <nil.venturas@campus.xaloc.org>
2025-04-04 20:49:27 +0200

    Update ejercicio_46.py

--- PHYTON/ejercicios_for_python/ejercicio_46.py
+++ PHYTON/ejercicios_for_python/ejercicio_46.py
@@ -1,12 +1,15 @@
-# Solucionar el error con la palabra 'Abrigo'
+#46. A partir del programa anterior, soluciona el error que se produce en el test anterior con la palabra Abrigo utilizando únicamente una instrucción.
 palabra = input("Introduce una palabra: ")
 vocales = "aeiouAEIOU"
+
 vocales_lista = ""
 consonantes_lista = ""
-for letra in palabra.lower():  # Convertir a minúsculas
+
+for letra in palabra.lower():
     if letra in vocales:
         vocales_lista += letra
     else:
         consonantes_lista += letra
+        
 print(f"Las vocales de la palabra {palabra} son: {vocales_lista}")
 print(f"Las consonantes de la palabra {palabra} son: {consonantes_lista}")
