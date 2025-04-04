a693e2679f1a3d2f2e540d4d9aa58f8c4f09713c
Nilventu <nil.venturas@campus.xaloc.org>
2025-04-04 20:52:19 +0200

    Update ejercicio_48.py

--- PHYTON/ejercicios_for_python/ejercicio_48.py
+++ PHYTON/ejercicios_for_python/ejercicio_48.py
@@ -1,8 +1,10 @@
-# Comprobar letras en la palabra secreta
-palabra_secreta = input("Introduce la palabra secreta: ")
-for i in range(len(palabra_secreta)):
+# 48. Realiza un programa que introduzcas por teclado una palabra ‘secreta’, consigue la longitud deesa palabra para que sea ese el criterio que establezca el rango del bucle de manera que el usuariotenga x oportunidades para ver si letra introducida está en esa palabra.
+
+palabra = input("Introduce la palabra : ")
+for i in range(len(palabra)):
     letra = input("Introduce una letra: ")
-    if letra in palabra_secreta:
+    if letra in palabra:
         print("La letra existe")
+
     else:
         print("La letra no existe")
