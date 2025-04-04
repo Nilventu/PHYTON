a37e010724edaad6fd3b46707f1fead3f1666abf
Nilventu <nil.venturas@campus.xaloc.org>
2025-04-04 12:00:18 +0200

    Update ejercicio_35.py

--- PHYTON/ejercicios_for_python/ejercicio_35.py
+++ PHYTON/ejercicios_for_python/ejercicio_35.py
@@ -1,5 +1,5 @@
-# Programa que repite tu nombre un número de veces introducido por el usuario
-nombre = "Alejandro De La Torre"
+# 35.Programa que al introducir un número por teclado permita mostrar ese número de veces tu nombre
+nombre = input("introduce un nombre: ")
 veces = int(input("Introduce el número de veces que quieres ver tu nombre: "))
 for _ in range(veces):
     print(nombre)
